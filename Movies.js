2c4b20bac0a5152f7ad0f0af5c243b70109e4c14
Lady_viktoria <viktoria212@mail.ru>
2020-05-26 16:35:24 +0300

    task3

--- Movies.js
+++ Movies.js
@@ -1,5 +1,14 @@
 'use strict';
-const numberOfFilms=+prompt ('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms=+prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms==''||numberOfFilms==null||isNaN(numberOfFilms)){
+        numberOfFilms=+prompt ('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 
 const personalMovieDB ={
     count: numberOfFilms,
@@ -10,7 +19,9 @@ const personalMovieDB ={
 
 };
 
-for (let i=0; i<2; i++) {
+function rememberMyFilms() {
+
+}for (let i=0; i<2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
         b = prompt('На сколько оцените его?', '');
 
@@ -22,14 +33,31 @@ for (let i=0; i<2; i++) {
             i--;
         }
 }
-if (personalMovieDB.count<10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >=10 &&personalMovieDB.count<30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count>=30) {
-    console.log("Вы киноман");
-} else {
-    console.log ("Произошла ошибка");
-}
-    console.log(personalMovieDB);
+rememberMyFilms();
 
+function detectPersonalLevel() {
+    if (personalMovieDB.count<10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >=10 &&personalMovieDB.count<30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count>=30) {
+        console.log("Вы киноман");
+    } else {
+        console.log ("Произошла ошибка");
+    }
+}
+detectPersonalLevel ();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres (){
+    for (let i=1; i<=3; i++) {
+        personalMovieDB.genres[i-1]=prompt(`Ваш любимый жанр под номером ${i}`);;
+    }
+}
+writeYourGenres();
